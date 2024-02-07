2f13df08dadb551c76f94f7935c8128587b8b708
Liam-HeatEng <140842289+Liam-HeatEng@users.noreply.github.com>
2024-02-07 21:07:41 +0000

    impl splash component

---
+++ client/components/splash.tsx
@@ -0,0 +1,8 @@
+export default function Splash() {
+    return (
+        <main className="flex flex-col justify-center items-center gap-4 p-12">
+            <h1 className="text-4xl font-bold"> GEOWARS </h1>
+            <button className="px-4 py-2 bg-green-700 text-amber-900 text-2xl"> </button>
+        </main>
+    )
+}
